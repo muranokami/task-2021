c88373ba28bf73709675dc5c48043eee839c2d87
muranokami <pikupiku68574.dj@gmail.com>
2021-01-19 11:18:43 +0900

    first commit

---
+++ app/models/task.rb
@@ -0,0 +1,53 @@
+class Task < ApplicationRecord
+    paginates_per 50
+    
+    def self.import(file)
+      CSV.foreach(file.path, headers: true) do |row|
+        task = new
+        task.attributes = row.to_hash.slice(*csv_attributes)
+        task.save!
+      end
+    end
+
+    def self.csv_attributes
+      ["name", "description", "created_at", "updated_at"]
+    end
+
+    def self.generate_csv
+      CSV.generate(headers: true) do |csv|
+        csv << csv_attributes
+        all.each do |task|
+          csv << csv_attributes.map{ |attr| task.send(attr) }
+        end
+      end 
+    end
+
+    has_one_attached :image
+    
+    def self.ransackable_attributes(auth_object = nil)
+      %w[name created_at]  
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+      []  
+    end
+
+    before_validation :set_nameless_name
+
+    scope :recent, -> { order(created_at: :desc) }
+
+    validates :name, presence: true, length: { maximum: 30 }
+    validate :validate_name_not_including_comma
+
+    belongs_to :user
+
+    private
+
+    def validate_name_not_including_comma
+      errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')  
+    end
+
+    def set_nameless_name
+      self.name = '名前なし' if name.blank?  
+    end
+end
